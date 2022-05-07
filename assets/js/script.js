708d0fd088f018a8cf771126b84e3d02d999b78e
David Yip <yipbqdav@gmail.com>
2022-05-07 00:32:12 -0700

    js
    am confuse

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const restartBtn = document.getElementById(restart)
+const question = document.getElementById(question)
+let currentQuestion = 0;
+
+let questions = [
+    {
+        question: "what is the microsfot equivalent of javascript?",
+        answers: [
+            {option1:"C#",answer:true},
+            {option2:"C-",answer:false},
+            {option3:"Java for Microsoft",answer:false},
+            {option4:"C++",answer:false},
+        ]
+    },
+    {
+        question: ""
+    }
+]
+function startGame() {
+    var score = 0;
+}
+function restartGame()
