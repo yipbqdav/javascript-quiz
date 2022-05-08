4520bedfe90131d765f7b6c6284b0ee28990c46f
yipbqdav <yipbqdav@gmail.com>
2022-05-07 23:52:32 -0700

    more work
    I need to just sit down and do this, it shouldn't be this hard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
-const restartBtn = document.getElementById(restart)
-const question = document.getElementById(question)
+generateBtn.addEventListener("click", restartGame);
 let currentQuestion = 0;
 
 let questions = [
     {
-        question: "what is the microsfot equivalent of javascript?",
+        question: "what was the original name of javascript?",
         answers: [
-            {option1:"C#",answer:true},
-            {option2:"C-",answer:false},
-            {option3:"Java for Microsoft",answer:false},
-            {option4:"C++",answer:false},
+            {option1:"mocha",answer:true},
+            {option2:"latte",answer:false},
+            {option3:"espresso",answer:false},
+            {option4:"java",answer:false},
         ]
     },
+
     {
-        question: ""
+        question: "what kind of language is javascript?",
+        answers: [
+            {option1:"object based",answer:true},
+            {option2:"logical",answer:false},
+            {option3:"procedural",answer:false},
+            {option4:"object oriented",answer:false}
+        ]
     }
 ]
-function startGame() {
-    var score = 0;
+function playGame() {
+    var questions = '';
+    for (let i = 0; i < questions; i++){
+        
+    }
 }
-function restartGame()
